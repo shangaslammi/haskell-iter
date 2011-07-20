92e0fbf68e1650e1e8bb7b7a7937dd845f25eaaf
Sami Hangaslammi <shang@iki.fi>
2011-07-20 10:14:28 +0300

    added out-of-order iteration test for split halves

--- test/test_iter.hs
+++ test/test_iter.hs
@@ -14,48 +14,48 @@ tests =
         let i = iterList l
         l' <- toList i
         l' @?= l
-    
+
     ,"test imap" ~: do
         let l = [1..5]
         let i = imap (*2) $ iterList l
         l' <- toList i
         l' @?= map (*2) l
-    
+
     ,"test ifilter" ~: do
         let l = [1..10]
         let i = ifilter odd $ iterList l
         l' <- toList i
         l' @?= filter odd l
-    
+
     ,"test +++" ~: do
         let i = iterList [1..3]
         let j = iterList [4..6]
         let k = iterList [7..10]
-        
+
         let i' = i +++ j +++ k
         l <- toList i'
         l @?= [1..10]
-        
+
     ,"test iconcat" ~: do
         let i = iterList $ map iterList ["foo", "bar", "asdf"]
         l <- toList $ iconcat i
         l @?= "foobarasdf"
-        
+
     ,"test iconcatMap" ~: do
         let i = iconcatMap iterList $ iterList ["foo", "bar", "asdf"]
         l <- toList i
         l @?= "foobarasdf"
-        
+
     ,"test itake and idrop" ~: do
         let i = itake 10 $ idrop 10 $ iterList [1..]
         l <- toList i
         l @?= [11..20]
-        
+
     ,"test ireverse" ~: do
         let i = ireverse $ itake 10 $ iterList [1..]
         l <- toList i
         l @?= reverse [1..10]
-        
+
     ,"test iintersperse0" ~: do
         let i = iintersperse '.' $ iterList ""
         l <- toList i
@@ -70,26 +70,34 @@ tests =
         let i = iintersperse '.' $ iterList "abc"
         l <- toList i
         l @?= "a.b.c"
-    
+
     ,"test iterFile" ~: do
         let i = iterFile testFilePath
         l <- toList i
         l @?= "one\ntwo\nthree"
-        
+
     ,"test finalizer" ~: do
         fh <- openFile testFilePath ReadMode
         let i = itake 3 $ iterHandle fh
         l <- toList i
         l @?= "one"
         hIsClosed fh >>= (@? "handle closed succesfully after iteration")
-    
+
     ,"test isplitAt" ~: do
         let i = isplitAt 3 $ iterList [1..10]
         (j,k) <- ihead i
         toList j >>= (@?= [1..3])
         toList k >>= (@?= [4..10])
+
+    ,"test isplitAt handle with out-of-order iteration" ~: do
+        fh <- openFile testFilePath ReadMode
+        let i = isplitAt 3 $ iterHandle fh
+        (j,k) <- ihead i
+        let k' = itake 3 $ idrop 1 k
+        toList k' >>= (@?= "two")
+        toList j  >>= (@?= "one")
+        hIsClosed fh >>= (@? "handle closed succesfully after iteration")
     ]
-        
+
 main = runTestTT $ test tests
 
-        
