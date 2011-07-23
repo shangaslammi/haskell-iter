1ca56d79a1a724f4b327349de16d8ddd2b400b91
Sami Hangaslammi <shang@iki.fi>
2011-07-23 19:06:53 +0300

    added test for peekNext

--- test/test_state.hs
+++ test/test_state.hs
@@ -47,6 +47,18 @@ tests =
             return (a,b,c,d)
 
         runIterIO s i >>= (@?= (Just 1, Just 2, Just 3, Nothing))
+    
+    ,"test peekNext" ~: do
+        let i = iterList [1,2]
+        let s = do
+            a <- peekNext
+            b <- tryNext
+            c <- peekNext
+            d <- tryNext
+            e <- peekNext
+            return (a,b,c,d,e)
+            
+        runIterIO s i >>= (@?= (Just 1, Just 1, Just 2, Just 2, Nothing))
     ]
 
 main = runTestTT $ test tests
