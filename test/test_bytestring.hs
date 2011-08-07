33118f8cbd22e62edeccdc0dfc91d445edb67b5b
Sami Hangaslammi <shang@iki.fi>
2011-08-07 20:35:29 +0300

    modified test to use toByteString

--- test/test_bytestring.hs
+++ test/test_bytestring.hs
@@ -13,8 +13,8 @@ testFilePath = "../test/testfile.bin"
 tests =
     ["iter binary file" ~: do
         let i = iterFile testFilePath
-        l <- fmap (B.unpack . B.concat) $ toList i
-        l @?= [1..9]
+        b <- fmap B.unpack $ toByteString i
+        b @?= [1..9]
     ]
 
 main = runTestTT $ test tests
