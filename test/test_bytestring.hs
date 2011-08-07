40e7841c18f82bb24919c24006d5681ed6c8076c
Sami Hangaslammi <shang@iki.fi>
2011-08-07 21:13:58 +0300

    added test for takeBytes

--- test/test_bytestring.hs
+++ test/test_bytestring.hs
@@ -11,10 +11,14 @@ import System.IO
 testFilePath = "../test/testfile.bin"
 
 tests =
-    ["iter binary file" ~: do
+    ["test iter binary file" ~: do
         let i = iterFile testFilePath
         b <- fmap B.unpack $ toByteString i
         b @?= [1..9]
+
+    ,"test takeBytes" ~: do
+        let i = bytes $ takeBytes 3 $ iterFile testFilePath
+        toList i >>= (@?= [1,2,3])
     ]
 
 main = runTestTT $ test tests
